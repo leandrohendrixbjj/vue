9ff3871d0ef4e8bcad284175f9d47bdfecbf5e45
leandro starks <leandrotully@gmail.com>
2025-04-03 19:59:58 -0300

    Style

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,94 +1,90 @@
 <template>
   <div class="container">
     <h1 class="titulo">{{ titulo }}</h1>
-    <ul class="lista-fotos">
-      <li class="li-lista-fotos" v-for="foto in fotos">
+    <ul class="fotos">
+      <li v-for="foto in fotos">
         <div class="painel">
           <h2 class="painel-titulo">{{ foto.titulo }}</h2>
           <div class="painel-conteudo">
-            <img class="imagem-responsiva"  :src="foto.url" :alt="foto.titulo" />
+            <img class="painel-img" :src="foto.url" :alt="foto.titulo" />
           </div>
-        </div>        
+        </div>
       </li>
-    </ul>  
+    </ul>
   </div>
 </template>
 
 <script>
-   export default {
-    data() {
-      return {
-        titulo:'AluraPic',
-        fotos: []
-      }
-    },
-    created() {
-      fetch('http://localhost:3000/v1/fotos')
-      .then(res => res.json())      
-      .then(data => {
-        this.fotos = data
+export default {
+  data() {
+    return {
+      titulo: "AluraPic",
+      fotos: []
+    };
+  },
+  created() {
+    fetch("http://localhost:3000/v1/fotos")
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        this.fotos = data;
       })
-    }
-   }
+      .catch((error) => console.error("Error fetching dog images:", error));
+  }
+};
 </script>
 
-<style scoped>
- * {
+<style>
+* {
   margin: 0;
   padding: 0;
- }
- 
- .container {    
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;    
-    width: 70%;
-    margin: auto;
-    font-family: Helvetica, sans-serif;
-  }
-  
-  .titulo {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;     
-  }
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 60%;
+  margin: auto;
+}
 
-  .lista-fotos {
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;    
-    list-style: none;
-    padding: 0;
-  }
+.titulo {
+  text-align: center;
+  font-size: 2em;
+  margin-bottom: 20px;
+}
 
-  .li-lista-fotos{
-    padding: 5px ;
-  }
+.fotos {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+  list-style: none;
+}
 
-  .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;  
+  vertical-align: top;
+  text-align: center;
+}
 
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 0 0;
-    padding: 10px;
-    text-transform: uppercase;
-  }
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
 
-  .imagem-responsiva {
-    width: 100%;
-  }
+.painel-img {
+  width: 100%;
+  max-width: 200px;
+  border-radius: 8px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+}
 </style>
