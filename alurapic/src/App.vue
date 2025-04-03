2bb85f1cf9bf478202777ace5596d1d0e374a780
leandro starks <leandrotully@gmail.com>
2025-04-03 20:49:17 -0300

    Componentes

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -3,12 +3,9 @@
     <h1 class="titulo">{{ titulo }}</h1>
     <ul class="fotos">
       <li v-for="foto in fotos">
-        <div class="painel">
-          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
-          <div class="painel-conteudo">
-            <img class="painel-img" :src="foto.url" :alt="foto.titulo" />
-          </div>
-        </div>
+        <meu-painel :titulo="foto.titulo">
+          <img class="painel-img" :src="foto.url" :alt="foto.titulo" />
+        </meu-painel>
       </li>
     </ul>
   </div>
@@ -39,6 +36,7 @@ export default {
   margin: 0;
   padding: 0;
 }
+
 .container {
   display: flex;
   flex-direction: column;
@@ -59,32 +57,5 @@ export default {
   justify-content: center;
   gap: 10px;
   list-style: none;
-}
-
-.painel {
-  padding: 0 auto;
-  border: solid 2px grey;
-  display: inline-block;
-  margin: 5px;
-  box-shadow: 5px 5px 10px grey;
-  width: 200px;  
-  vertical-align: top;
-  text-align: center;
-}
-
-.painel .painel-titulo {
-  text-align: center;
-  border: solid 2px;
-  background: lightblue;
-  margin: 0;
-  padding: 10px;
-  text-transform: uppercase;
-}
-
-.painel-img {
-  width: 100%;
-  max-width: 200px;
-  border-radius: 8px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
 }
 </style>
---
+++ alurapic/src/components/shared/painel/Painel.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="painel">
+    <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+    <div class="painel-conteudo">
+      <img class="painel-img" :src="foto.url" :alt="foto.titulo" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+
+.painel-img {
+  width: 100%;
+  max-width: 200px;
+  border-radius: 8px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+}
+</style>
