ae0926e3c20b1793ac886d28d3977910dbe6fc41
leandro starks <leandrotully@gmail.com>
2025-05-26 19:08:32 -0300

    Menu como componenete

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="container">
-    <nav class="navbar">
-      <ul class="nav-list">
-        <li class="nav-item" v-for="route in routes" :key="route.path">
-          <router-link :to="route.path" class="nav-link">{{ route.name }}</router-link>
-        </li>        
-      </ul>
-    </nav>
+  <div class="container">    
+    <meu-menu :routes="routes" />
     <router-view />
   </div>
 </template>
 
 <script>
 import { routes } from './routes.js'
+import Menu from './components/shared/menu/Menu.vue'
+
 export default {
+  components: {
+    'meu-menu': Menu,    
+  },
+
   data() {
     return {
       routes,
@@ -35,32 +35,4 @@ export default {
   padding: 20px;
 }
 
-.navbar {
-  background-color: #1e293b; /* azul-escuro */
-  padding: 15px 20px;
-  border-radius: 8px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-}
-
-.nav-list {
-  list-style: none;
-  display: flex;
-  gap: 20px;
-  justify-content: flex-start;
-}
-
-.nav-link {
-  text-decoration: none;
-  color: #f1f5f9; 
-  font-weight: 500;
-  font-size: 16px;
-  transition: color 0.3s ease, border-bottom 0.3s ease;
-  padding-bottom: 2px;
-  border-bottom: 2px solid transparent;
-}
-
-.nav-link:hover {
-  color: #38bdf8; 
-  border-bottom: 2px solid #38bdf8;
-}
 </style>
---
+++ alurapic/src/components/shared/menu/Menu.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="navbar">
+    <ul class="nav-list">
+      <li class="nav-item" v-for="route in routes" :key="route.path">
+        <router-link :to="route.path" class="nav-link">{{ route.name }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'Menu',
+    props: { 
+      routes: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .navbar {
+    background-color: #1e293b;    
+    padding: 15px 20px;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  }
+
+  .nav-list {
+    list-style: none;
+    display: flex;
+    gap: 20px;
+    justify-content: flex-start;
+  }
+
+  .nav-link {
+    text-decoration: none;
+    color: #f1f5f9;
+    font-weight: 500;
+    font-size: 16px;
+    transition: color 0.3s ease, border-bottom 0.3s ease;
+    padding-bottom: 2px;
+    border-bottom: 2px solid transparent;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  .nav-link:hover {
+    color: #38bdf8;
+    border-bottom: 2px solid #38bdf8;
+  }
+</style>
